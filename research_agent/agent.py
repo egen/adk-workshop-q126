7977dcbd4cb2bee8c3eb261172f2ff8094ef063e
Saurabh Gupta <saurabh.gupta@peapoddigitallabs.com>
2026-02-18 10:55:42 -0500

    changed the agent to gemini-3-pro-preview

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -29,7 +29,7 @@ from google.adk.agents import Agent
 
 root_agent = Agent(
     name="Simple_research_Agent",
-    model="gemini-2.0-flash",
+    model="gemini-3-pro-preview",
     instruction="""
     Your are a helpful research assistant. Your goal is to help users find information and answer questions.
 
