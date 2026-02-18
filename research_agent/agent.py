a85f9ba38763e56c60357786fd27e164311d4730
Tharun Adla <tharunadla@mac.home>
2026-02-18 11:04:22 -0500

    Updated model

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -29,7 +29,7 @@ from google.adk.agents import Agent
 
 root_agent = Agent(
     name="Simple_research_Agent",
-    model="gemini-2.0-falsh",
+    model="gemini-2.0-flash",
     instruction="""
     You are a helpful research assistant. Your goal is to help users
     find information and answer question.
