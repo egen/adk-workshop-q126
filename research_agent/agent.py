1212434435aac25af738e9a70cae1556b9008999
Rishabh Jain <rishabh@cloudy.local>
2026-02-18 07:49:08 -0800

    update agent.py

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -28,7 +28,7 @@ from google.adk.agents import Agent
 # )
 
 root_agent = Agent(
-    name="Mímir",
+    name="Mimir",
     model="gemini-2.0-flash",
     instruction="""
     You are a helpful research assistant. Your goal is to help users find information and answer questions.
