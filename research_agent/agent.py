29a268d2501860dfe964fbd09c86ca8051196ca7
Ritvik Kondabathini <ritvikkondabathini@Ritviks-MacBook-Pro-2.local>
2026-02-18 09:55:06 -0600

    fix

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -27,9 +27,9 @@ from google.adk.agents import Agent
 #     description="...",
 # )
 
-root_agent = Agent{
+root_agent = Agent(
     name="Simple Research Agent - Ritvik",
     model="gemini-2.0-flash",
     instruction="You are a simple research agent. Your job is to help user find information and data. At this point of time, you do not have access to any tools.",
     description="Simple Agent",
-}
+)
