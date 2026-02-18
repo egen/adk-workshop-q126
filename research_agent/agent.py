80c46f2f1148803e9bf996a534efebc2aceca315
Sam Gallagher <gallags6@gene.com>
2026-02-18 10:54:15 -0600

    using fetch_webpage tool

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -10,7 +10,7 @@ Current Exercise: 1 (Simple Single Agent)
 """
 
 from google.adk.agents import Agent
-from google.adk.tools import google_search
+from .tools import fetch_webpage
 
 
 root_agent = Agent(
@@ -20,11 +20,11 @@ root_agent = Agent(
     You are a helpful research assistant. Your goal is to help users
     find information and answer questions.
 
-    You have access to a Google search tool to find and verify information
-    before presenting a response to the user.
+    You have access to a fetch_webpage tool, use this to collect
+    more information about the topic before responding to the user.
     """,
     description="Simple research agent",
     tools=[
-        google_search
+        fetch_webpage
     ]
 )
