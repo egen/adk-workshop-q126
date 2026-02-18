71033d67b02047a84a6a43ffe7e586881b2c1bdd
akanshashrivastava-bit <akansha.shrivastava@egen.ai>
2026-02-18 21:18:54 +0530

    added agent

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -28,7 +28,7 @@ from google.adk.agents import Agent
 # )
 
 root_agent = Agent(
-    name="simple agent tool"
+    name="basic_research_agent_tool"
     model="gemini-2.0-flash"
     instruction="""
     You are a helpful research assistant. Your goal is to help users
