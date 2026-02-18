a498f230c2c32e55397648b38c49f412a0b5fa18
Sarath Kedarisetty <sarath.chandra@springml.com>
2026-02-18 11:43:31 -0500

    Adding the Search tool to Agent

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -10,6 +10,7 @@ Current Exercise: 1 (Simple Single Agent)
 """
 
 from google.adk.agents import Agent
+from google.adk.tools import google_search
 
 # Exercise 1: Create a simple research agent
 # TODO: Define your root_agent here
@@ -30,6 +31,10 @@ from google.adk.agents import Agent
 root_agent = Agent(
     name="Recipe_Researcher",
     model="gemini-2.0-flash",
-    instruction="You are a recipe research assistant that helps users find information about recipes.",
-    description="An agent specialized in helping with research tasks."
+    instruction="""You are a recipe research assistant that helps users find information about recipes. 
+    You haveaccess to a google search tool that you can use to find information on the web.
+    When a user asks you a question, you should first try to answer it based on your existing knowledge. If you don't know the answer, or if you think you can find a better answer by searching the web, you should use the google search tool to find the information. 
+    After using the google search tool, you should use the information you found to answer the user's question.""",
+    description="An agent specialized in helping with research tasks.",
+    tools=[google_search]
 )
