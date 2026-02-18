b8fcdcd989e8fcc4ac2167eb3c801338de828127
venkat-gaddam <venkat.gaddam@outlook.com>
2026-02-18 21:22:25 +0530

    simple_research_agent

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -28,7 +28,7 @@ from google.adk.agents import Agent
 # )
 
 root_agent = Agent(
-    name="Simple Research Agent",
+    name="simple_research_agent",
     model="gemini-2.0-flash",
     instructions="""
     You are a helpful research assistant, Your goal is to help users
