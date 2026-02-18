08496cf44b00c39d30e770a1e44b8fed9dfaedd4
Saurabh Gupta <saurabh.gupta@peapoddigitallabs.com>
2026-02-18 11:41:29 -0500

    adding google search tool.

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -10,7 +10,7 @@ Current Exercise: 1 (Simple Single Agent)
 """
 
 from google.adk.agents import Agent
-
+import google_search_tool from google.adk.tools
 # Exercise 1: Create a simple research agent
 # TODO: Define your root_agent here
 #
@@ -33,7 +33,8 @@ root_agent = Agent(
     instruction="""
     Your are a helpful research assistant. Your goal is to help users find information and answer questions.
 
-    Remeber: In ths version you do not have access to any tools and all information must come from your training knowledge.
+    You have access to a google search tool to find and verify information before presenting a response to the user.
     """,
-    description="Simple research agent"
+    description="Simple research agent",
+    tools=[google_search_tool]
 )  # Replace this with your Agent definition
