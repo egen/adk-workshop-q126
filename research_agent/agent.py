c841690dc5d00363876e40a3c11aef1d72fc9211
Pardha Bharadwaj <84728178+PardhaBharadwajsml@users.noreply.github.com>
2026-02-18 21:36:58 +0530

    simple-agent-model-change

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -30,8 +30,8 @@ from google.adk.agents import Agent
 # root_agent = None  # Replace this with your Agent definition
 
 root_agent = Agent(
-    name="Simple_research_agent",
-    model="gemini-3-pro-preview",
+    name="simple_research_agent",
+    model="gemini-2.0-flash",
     instruction="""
     You are a helpful research assistant. Your goal is to help user's find information and answer questions.
 
