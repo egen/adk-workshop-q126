2236f3a6fee7e5f97e721ac5fdd865d07965ee96
chaitanyakasaraneni <24287691+chaitanyakasaraneni@users.noreply.github.com>
2026-02-18 10:46:39 -0600

    google search

--- research_agent/first_ex_agent.py
+++ research_agent/first_ex_agent.py
@@ -10,21 +10,16 @@ Current Exercise: 1 (Simple Single Agent)
 """
 
 from google.adk.agents import Agent
-from google.adk.tools import google_search
-
 
 root_agent = Agent(
     name="simple_research_agent",
     model="gemini-2.0-flash",
     instruction="""
-    You are a helpful research assistant. Your goal is to help users
-    find information and answer questions.
+        You are a helpful research assistant. Your goal is to help users
+        find information and answer questions.
 
-    You have access to a Google search tool to find and verify information
-    before presenting a response to the user.
-    """,
-    description="Simple research agent",
-    tools=[
-        google_search
-    ]
+        Remember: In this version you do not have access to any tools and
+        all information must come from your training knowledge.""",
+
+    description="A simple agent that helps with basic research tasks.",
 )
