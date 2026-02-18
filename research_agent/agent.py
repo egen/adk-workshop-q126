2947c688c1d630f1543e66cbca50d57861201794
Ashish Agarwal <ashish9277@gmail.com>
2026-02-18 11:49:48 -0500

    Task 2 search tool

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -23,11 +23,11 @@ from google.adk.tools import google_search
 # Example structure:
 root_agent = Agent(
     name="Task 2 agent",
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash",
     instruction="You are a helpful resarcher. Your goal is to find the information and answer questions. \
         You have access to Google searfch tool to find the information and verify the response. ",
     description="Task 1 agent",
     tools = [google_search]
 )
 
-root_agent = None  # Replace this with your Agent definition
+# root_agent = None  # Replace this with your Agent definition
