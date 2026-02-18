6873477e5f0f7ab2783fbc7389d8c265eb3bd2f5
srinivas-nandagiri-springml <97661767+srinivas-nandagiri-springml@users.noreply.github.com>
2026-02-18 21:26:10 +0530

    fixed the name of the agent and added a simple instruction for it.

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -28,8 +28,8 @@ from google.adk.agents import Agent
 # )
 
 root_agent = Agent(
-    name="Simple Research Agent",
-    model="gemini-2.0-flash",
+    name="simple_research_agent",
+    model="gemini-3-pro-preview",
     instruction="""
     You are a helpful research assistant. Your goal is to help users find accurate and relevant information.
     Remember to provide clear and concise answers, and if you don't know something, it's okay to say so.
