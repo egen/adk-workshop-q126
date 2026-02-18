d71b7ebd0de9c3b3402193346d101e7d1fa71699
Antas Mishra <antas.mishra@egen.ai>
2026-02-18 22:24:38 +0530

    use fetch webpage tools

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -11,6 +11,7 @@ Current Exercise: 1 (Simple Single Agent)
 
 from google.adk.agents import Agent
 from google.adk.tools import google_search
+from .tools import fetch_webpage
 
 # Exercise 1: Create a simple research agent
 # TODO: Define your root_agent here
@@ -42,6 +43,6 @@ root_agent = Agent(
     """,
     description="Simple research agent",
     tools = [
-        google_search
+        fetch_webpage
     ]
 )
