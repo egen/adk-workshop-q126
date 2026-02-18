8399c12efc5f880ef918371498266585b48fbe1c
syedakbar <amahmood@qarik.com>
2026-02-18 07:44:08 -0800

    add simple research agent

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -27,4 +27,15 @@ from google.adk.agents import Agent
 #     description="...",
 # )
 
-root_agent = None  # Replace this with your Agent definition
+root_agent = Agent(
+    name = "simple research agent",
+    model = "gemini-2.0-flash",
+    instructions="""
+    You are a helpful research assistant. Your goal is to help users
+    find information and answer questions.
+
+    Remember: In this version you do not have access to any tools and
+    all information must come from your training knowledge.
+    """,
+    description = "simple research agent"
+)  # Replace this with your Agent definition
