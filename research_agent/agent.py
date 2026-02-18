5b3a4f2c15ab6edc1bfc1026317e66b8a966036e
Saurabh Gupta <saurabh.gupta@peapoddigitallabs.com>
2026-02-18 10:50:41 -0500

    Simple research agent fixed agent name

--- research_agent/agent.py
+++ research_agent/agent.py
@@ -28,7 +28,7 @@ from google.adk.agents import Agent
 # )
 
 root_agent = Agent(
-    name="Simple research Agent",
+    name="Simple_research_Agent",
     model="gemini-2.0-flash",
     instruction="""
     Your are a helpful research assistant. Your goal is to help users find information and answer questions.
